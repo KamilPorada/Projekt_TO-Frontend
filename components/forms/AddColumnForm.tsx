cba98bae27c8c9e1ae582c2e52d12bf715f958c3
Kamil0k <kamil.porada@onet.pl>
2024-04-19 14:50:47 +0200

    validaion new table form

--- components/forms/AddColumnForm.tsx
+++ components/forms/AddColumnForm.tsx
@@ -35,6 +35,7 @@ const AddColumnForm: React.FC<Props> = ({ onAddColumn }) => {
 	}
 
 	const [newColumn, setNewColumn] = useState<TableColumn>(initialColumnState)
+	const [errors, setErrors] = useState<any>({})
 
 	const variableTypes = [
 		'integer',
@@ -51,17 +52,51 @@ const AddColumnForm: React.FC<Props> = ({ onAddColumn }) => {
 	]
 
 	const handleChange = (field: keyof TableColumn, value: any) => {
-        setNewColumn(prevState => ({
-            ...prevState,
-            [field]: value,
-        }));
-    };
-    
-    
+		setNewColumn(prevState => ({
+			...prevState,
+			[field]: value,
+		}))
+	}
 
 	const handleAddColumn = () => {
-		onAddColumn(newColumn)
-		setNewColumn(initialColumnState)
+		const validationErrors: any = {}
+
+		// Walidacja nazwy kolumny
+		if (!newColumn.fieldName.trim()) {
+			validationErrors.fieldName = 'Nazwa kolumny jest wymagana'
+		}
+
+		// Walidacja typu danych
+		if (!newColumn.fieldType) {
+			validationErrors.fieldType = 'Typ danych jest wymagany'
+		}
+
+		// Walidacja dodatkowych inputów w zależności od wybranego typu danych
+		if (newColumn.fieldType === 'decimal') {
+			if (newColumn.fieldSize1 <= 0 || newColumn.fieldSize2 < 0) {
+				validationErrors.fieldSize = 'Podaj poprawne wartości dla liczby cyfr całkowitych i dziesiętnych miejsc'
+			}
+		} else if (newColumn.fieldType === 'char' || newColumn.fieldType === 'varchar') {
+			if (newColumn.fieldSize1 <= 0) {
+				validationErrors.fieldSize = 'Podaj poprawną długość łańcucha'
+			}
+		}
+
+		if (newColumn.isForeignKey === true) {
+			if (!newColumn.foreignTable || !newColumn.foreignField) {
+				validationErrors.foreignKey = 'Wypełnij nazwę tabeli i kolumny dla klucza obcego'
+			}
+		}
+
+		if (Object.keys(validationErrors).length === 0) {
+			// Brak błędów, dodaj kolumnę
+			onAddColumn(newColumn)
+			setNewColumn(initialColumnState)
+			setErrors({})
+		} else {
+			// Wystąpiły błędy, ustaw błędy i zaznacz nieprawidłowe pola
+			setErrors(validationErrors)
+		}
 	}
 
 	return (
@@ -75,34 +110,48 @@ const AddColumnForm: React.FC<Props> = ({ onAddColumn }) => {
 					value={newColumn.fieldName}
 					onChange={e => handleChange('fieldName', e.target.value)}
 				/>
+				{errors.fieldName && <span className='text-red-500 text-xs'>{errors.fieldName}</span>}
 			</div>
 			<div className='flex flex-col w-full mt-3 text-sm'>
 				<label>Typ zmiennej:</label>
 				<select
 					value={newColumn.fieldType}
 					onChange={e => {
-                        if(e.target.value !== 'decimal'){
-                            setNewColumn(prevState => ({
-                                ...prevState,
-                                fieldSize1: 0,
-                                fieldSize2: 0
-                            }));
-                        } 
-                        if(e.target.value !== 'char'){
-                            setNewColumn(prevState => ({
-                                ...prevState,
-                                fieldSize1: 0,
-                                fieldSize2: 0
-                            }));
-                        } 
-                        if(e.target.value !== 'varchar'){
-                            setNewColumn(prevState => ({
-                                ...prevState,
-                                fieldSize1: 0,
-                                fieldSize2: 0
-                            }));
-                        }
-                        handleChange('fieldType', e.target.value)}}
+						if (e.target.value !== 'decimal') {
+							setNewColumn(prevState => ({
+								...prevState,
+								fieldSize1: 0,
+								fieldSize2: 0,
+							}))
+						}
+						if (e.target.value !== 'char') {
+							setNewColumn(prevState => ({
+								...prevState,
+								fieldSize1: 0,
+								fieldSize2: 0,
+							}))
+						}
+						if (e.target.value !== 'varchar') {
+							setNewColumn(prevState => ({
+								...prevState,
+								fieldSize1: 0,
+								fieldSize2: 0,
+							}))
+							if (e.target.value !== 'integer') {
+								setNewColumn(prevState => ({
+									...prevState,
+									isAutoincrement: false,
+								}))
+							}
+							if (e.target.value === 'bool') {
+								setNewColumn(prevState => ({
+									...prevState,
+									isPrimaryKey: false,
+								}))
+							}
+						}
+						handleChange('fieldType', e.target.value)
+					}}
 					className='p-1 mt-1 bg-gray-100 rounded-sm shadow-md focus:outline-mainColor'>
 					<option value=''>Wybierz typ...</option>
 					{variableTypes.map((type, idx) => (
@@ -111,6 +160,7 @@ const AddColumnForm: React.FC<Props> = ({ onAddColumn }) => {
 						</option>
 					))}
 				</select>
+				{errors.fieldType && <span className='text-red-500 text-xs'>{errors.fieldType}</span>}
 			</div>
 			{newColumn.fieldType === 'decimal' && (
 				<div className='flex flex-col justify-center w-full'>
@@ -130,6 +180,7 @@ const AddColumnForm: React.FC<Props> = ({ onAddColumn }) => {
 							onChange={e => handleChange('fieldSize2', parseInt(e.target.value))}
 						/>
 					</div>
+					{errors.fieldSize && <span className='text-red-500 text-xs'>{errors.fieldSize}</span>}
 				</div>
 			)}
 
@@ -141,6 +192,7 @@ const AddColumnForm: React.FC<Props> = ({ onAddColumn }) => {
 						className='p-1 mt-1 bg-gray-100 rounded-sm shadow-md focus:outline-mainColor'
 						onChange={e => handleChange('fieldSize1', parseInt(e.target.value))}
 					/>
+					{errors.fieldSize && <span className='text-red-500 text-xs'>{errors.fieldSize}</span>}
 				</div>
 			)}
 
@@ -150,7 +202,16 @@ const AddColumnForm: React.FC<Props> = ({ onAddColumn }) => {
 					type='checkbox'
 					className='p-1 mt-1 bg-gray-100'
 					checked={newColumn.isPrimaryKey}
-					onChange={() => handleChange('isPrimaryKey', !newColumn.isPrimaryKey)}
+					disabled={newColumn.fieldType === 'bool'}
+					onChange={() => {
+						if (newColumn.fieldType === 'integer') {
+							setNewColumn(prevState => ({
+								...prevState,
+								isAutoincrement: true,
+							}))
+						}
+						handleChange('isPrimaryKey', !newColumn.isPrimaryKey)
+					}}
 				/>
 			</div>
 
@@ -161,19 +222,19 @@ const AddColumnForm: React.FC<Props> = ({ onAddColumn }) => {
 					className='p-1 mt-1 bg-gray-100'
 					checked={newColumn.isForeignKey}
 					onChange={() => {
-                        if(!newColumn.isForeignKey === false){
-                            console.log("Jestem tutaj!")
-                            setNewColumn(prevState => ({
-                                ...prevState,
-                                foreignTable: '',
-                                foreignField: ''
-                            }));
-                        }
-                        handleChange('isForeignKey', !newColumn.isForeignKey)}}
+						if (!newColumn.isForeignKey === false) {
+							setNewColumn(prevState => ({
+								...prevState,
+								foreignTable: '',
+								foreignField: '',
+							}))
+						}
+						handleChange('isForeignKey', !newColumn.isForeignKey)
+					}}
 				/>
 			</div>
 			{newColumn.isForeignKey && (
-				<div className='flex flex-row justify-between items-center gap-2'>
+				<div className='flex flex-col justify-between items-left  gap-2'>
 					<div className='flex flex-col w-full mt-3 text-sm'>
 						<label>Tabela</label>
 						<input
@@ -192,7 +253,9 @@ const AddColumnForm: React.FC<Props> = ({ onAddColumn }) => {
 							onChange={e => handleChange('foreignField', e.target.value)}
 						/>
 					</div>
+                    {errors.foreignKey && <span className='text-red-500 text-xs'>{errors.foreignKey}</span>}
 				</div>
+                
 			)}
 
 			<div className='flex flex-row justify-between items-center w-full mt-3 text-sm'>
@@ -201,6 +264,7 @@ const AddColumnForm: React.FC<Props> = ({ onAddColumn }) => {
 					type='checkbox'
 					className='p-1 mt-1 bg-gray-100'
 					checked={newColumn.isAutoincrement}
+					disabled={newColumn.fieldType !== 'integer'}
 					onChange={() => handleChange('isAutoincrement', !newColumn.isAutoincrement)}
 				/>
 			</div>
--- components/forms/NewTableForm.tsx
+++ components/forms/NewTableForm.tsx
@@ -1,76 +1,95 @@
 'use client'
-import React, { useState } from 'react';
-import Button from '../UI/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTable, faPlus } from '@fortawesome/free-solid-svg-icons';
-import AddColumnForm from './AddColumnForm';
+import React, { useState } from 'react'
+import Button from '../UI/Button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTable, faPlus } from '@fortawesome/free-solid-svg-icons'
+import AddColumnForm from './AddColumnForm'
 
 interface TableColumn {
-    fieldName: string;
-    fieldType: string;
-    fieldSize1: number;
-    fieldSize2: number;
-    isPrimaryKey: boolean;
-    isForeignKey: boolean;
-    foreignTable: string;
-    foreignField: string;
-    isAutoincrement: boolean;
-    isUnique: boolean;
-    isNotNull: boolean;
+	fieldName: string
+	fieldType: string
+	fieldSize1: number
+	fieldSize2: number
+	isPrimaryKey: boolean
+	isForeignKey: boolean
+	foreignTable: string
+	foreignField: string
+	isAutoincrement: boolean
+	isUnique: boolean
+	isNotNull: boolean
 }
 
 const NewTableForm: React.FC = () => {
-    const [tableName, setTableName] = useState<string>('');
-    const [columns, setColumns] = useState<TableColumn[]>([]);
-    const [showAddColumnForm, setShowAddColumnForm] = useState<boolean>(false);
+	const [tableName, setTableName] = useState<string>('')
+	const [columns, setColumns] = useState<TableColumn[]>([])
+	const [showAddColumnForm, setShowAddColumnForm] = useState<boolean>(false)
+	const [isTableNameEntered, setIsTableNameEntered] = useState<boolean>(false)
+	const [tableNameError, setTableNameError] = useState<string>('')
+	const [columnsError, setColumnsError] = useState<string>('')
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-    };
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		if (!isTableNameEntered) {
+			setTableNameError('Nazwa tabeli jest wymagana')
+			return
+		} else if (columns.length === 0) {
+			setColumnsError('Brak dodanych kolumn tabeli')
+		}
+		console.log(columns)
+	}
 
-    const handleAddColumn = (newColumn: TableColumn) => {
-        setColumns(prevColumns => [...prevColumns, newColumn]);
-        setShowAddColumnForm(false); 
-    };
+	const handleAddColumn = (newColumn: TableColumn) => {
+		setColumns(prevColumns => [...prevColumns, newColumn])
+		setShowAddColumnForm(false)
+	}
 
-    return (
-        <form
-            onSubmit={handleSubmit}
-            className='flex flex-col justify-center items-center w-full sm:w-4/5 md:w-3/5 bg-white rounded-md shadow-lg p-5'>
-            <div className='flex flex-col justify-center items-center text-center'>
-                <div className='flex flex-row justify-center items-center gap-4 text-mainColor mb-2'>
-                    <h2 className='text-xl md:text-2xl uppercase font-semibold'>Nowa tabela</h2>
-                    <FontAwesomeIcon icon={faTable} className='text-2xl md:text-3xl text-shadow' />
-                </div>
-                <h3 className='text-sm md:text-base font-semibold'>Tworzenie tabeli</h3>
-                <p className='text-sm md:text-base font-thin'>
-                    Uzupełnij nazwę tabeli a następnie poprzez znak plusa dodawaj kolejne kolumny do tabeli. Wybierz jej nazwę,
-                    typ danych oraz inne dodatkowe parametry!
-                </p>
-            </div>
-            <div className='flex flex-col w-full mt-3 text-sm'>
-                <label>Nazwa tabeli:</label>
-                <input
-                    type='text'
-                    className='p-1 mt-1 bg-gray-100 rounded-sm shadow-md focus:outline-mainColor'
-                    value={tableName}
-                    onChange={e => setTableName(e.target.value)}
-                />
-            </div>
-            <div className='flex flex-row justify-between items-center w-full mt-5'>
-                <p className='text-sm text-left'>Kolumny tabeli</p>
-                <Button className='flex justify-center items-center px-4' onClick={() => setShowAddColumnForm(true)}>
-                    <FontAwesomeIcon className='text-xs' icon={faPlus} />
-                </Button>
-            </div>
+	const handleTableNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value
+		setTableName(value)
+		setIsTableNameEntered(value.trim().length > 0)
+		setTableNameError(value.trim().length === 0 ? 'Nazwa tabeli jest wymagana' : '')
+	}
 
-            {showAddColumnForm && <AddColumnForm onAddColumn={handleAddColumn} />}
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className='flex flex-col justify-center items-center w-full sm:w-4/5 md:w-3/5 bg-white rounded-md shadow-lg p-5'>
+			<div className='flex flex-col justify-center items-center text-center'>
+				<div className='flex flex-row justify-center items-center gap-4 text-mainColor mb-2'>
+					<h2 className='text-xl md:text-2xl uppercase font-semibold'>Nowa tabela</h2>
+					<FontAwesomeIcon icon={faTable} className='text-2xl md:text-3xl text-shadow' />
+				</div>
+				<h3 className='text-sm md:text-base font-semibold'>Tworzenie tabeli</h3>
+				<p className='text-sm md:text-base font-thin'>
+					Uzupełnij nazwę tabeli a następnie poprzez znak plusa dodawaj kolejne kolumny do tabeli. Wybierz jej nazwę,
+					typ danych oraz inne dodatkowe parametry!
+				</p>
+			</div>
+			<div className='flex flex-col w-full mt-3 text-sm'>
+				<label>Nazwa tabeli:</label>
+				<input
+					type='text'
+					className={`p-1 mt-1 bg-gray-100 rounded-sm shadow-md focus:outline-mainColor ${
+						tableNameError && 'border-red-500'
+					}`}
+					value={tableName}
+					onChange={handleTableNameChange}
+				/>
+				{tableNameError && <span className='text-red-500 text-xs'>{tableNameError}</span>}
+			</div>
+			<div className='flex flex-row justify-between items-center w-full mt-5'>
+				<p className='text-sm text-left'>Kolumny tabeli</p>
+				<Button className='flex justify-center items-center px-4' onClick={() => setShowAddColumnForm(true)}>
+					<FontAwesomeIcon className='text-xs' icon={faPlus} />
+				</Button>
+			</div>
+			{columnsError && <span className='text-red-500 text-xs mt-2 text-left w-full'>{columnsError}</span>}
 
-            <Button  className='w-full mt-10' onClick={()=>{
-                console.log(columns)
-            }}>Stwórz nową tabelę</Button>
-        </form>
-    );
-};
+			{showAddColumnForm && <AddColumnForm onAddColumn={handleAddColumn} />}
 
-export default NewTableForm;
+			<Button className='w-full mt-10'>Stwórz nową tabelę</Button>
+		</form>
+	)
+}
+
+export default NewTableForm
